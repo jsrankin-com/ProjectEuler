c86ad7415f4d6fe23ebdf6ce61454f0792dfb4c5
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-04-01 02:12:28 -0400

    Create pe03.h
    Initial commit

---
+++ pe03.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+
+//https://projecteuler.net/problem=3
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+*/
+std::vector<long long> PrimeFactor(long long high)
+{
+	std::vector<long long> factors;
+
+
+	while ((high % 2) == 0) {
+		factors.push_back(2);
+		high /= 2;
+	}
+
+	while (high % 3 == 0) {
+		factors.push_back(3);
+		high /= 3;
+	}
+
+	for (long long i = 5; i <= sqrt(high); i += 6) {
+		while (high % i == 0) {
+			factors.push_back(i);
+			high /= i;
+		}
+		while (high % (i + 2) == 0) {
+			factors.push_back(i + 2);
+			high = high / (i + 2);
+		}
+	}
+
+	if (high > 4)
+		factors.push_back(high);
+
+	return factors;
+}
+
+long long LargestPrimeFactor(long long high)
+{
+	std::vector<long long> v = PrimeFactor(high);
+	return v.back();
+}
